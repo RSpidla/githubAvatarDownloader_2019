d02d0b0ed42d6855e7e5833d34f4315d761fc223
‘RSpidla’ <rspidla@gmail.com>
2019-02-12 02:31:32 +0000

    Updated code with step 5, added in request header to GET request

--- download_avatars.js
+++ download_avatars.js
@@ -10,11 +10,16 @@ console.log("Welcome to the Github Avatar Downloader!!!");
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
-  request(url, function(err, res, body) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+
+  request(options, function(err, res, body) {
     cb(err, body);
   });
-
 }
 
 
