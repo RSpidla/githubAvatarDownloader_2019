38b882b0c3fdc2cff774e23d775ba3c8ad79c5c1
‘RSpidla’ <rspidla@gmail.com>
2019-02-12 02:35:36 +0000

    Double checked code with step 6 and cleaned up code formatting.

--- download_avatars.js
+++ download_avatars.js
@@ -3,12 +3,6 @@ const KEYS = require('./secrets');
 
 console.log("Welcome to the Github Avatar Downloader!!!");
 
-
-
-
-
-
-
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
@@ -22,14 +16,7 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
-
-
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
-
-
-
-
-
